cc3b9b4e65439e7a2812f22ca5507762fa4d4cd6
youfang <981545521@qq.com>
2018-09-29 16:07:28 +0800

    add : 基础功能提交

---
+++ venus-main/src/main/java/cn/acyou/base/Application.java
@@ -0,0 +1,16 @@
+package cn.acyou.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-09-29 下午 03:55]
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
